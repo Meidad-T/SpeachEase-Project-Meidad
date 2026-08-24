ce1cf1de2f253f6d974759226f3c05a09d3e4f0d
Meidad Troper <meidadtr@gmail.com>
2026-08-24 10:56:00 -0500

    Initial commit with Xcode project setup

---
+++ SpeachEase-Project-MeidadTests/SpeachEase_Project_MeidadTests.swift
@@ -0,0 +1,18 @@
+//
+//  SpeachEase_Project_MeidadTests.swift
+//  SpeachEase-Project-MeidadTests
+//
+//  Created by Meidad Troper on 5/17/26.
+//
+
+import Testing
+
+struct SpeachEase_Project_MeidadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
