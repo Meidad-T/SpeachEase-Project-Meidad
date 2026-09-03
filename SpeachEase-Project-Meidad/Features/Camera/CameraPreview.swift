08004c79d993145ab5a635e7e97bf84c1a6be070
Meidad Troper <meidadtr@gmail.com>
2026-09-03 04:19:00 -0500

    Implement core layout and logic for CameraPreview.swift

--- SpeachEase-Project-Meidad/Features/Camera/CameraPreview.swift
+++ SpeachEase-Project-Meidad/Features/Camera/CameraPreview.swift
@@ -9,3 +9,27 @@ struct CameraPreview: UIViewControllerRepresentable {
 
     func updateUIViewController(_ uiViewController: CameraViewController, context: Context) {}
 }
+
+class CameraViewController: UIViewController {
+    var captureSession: AVCaptureSession?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCamera()
+    }
+    
+    // Fix layout when view bounds change (e.g. rotation)
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer?.frame = view.bounds
+    }
+    
+    func setupCamera() {
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = .photo
+        
+        guard let cameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
+            print("No front camera found")
+            return
+        }
